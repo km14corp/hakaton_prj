a7d3c559423671d64dd087e2b780af0c9d49a828
Danylo Borysenko <bdadaniil10@gmail.com>
2022-01-06 16:09:36 +0200

    bot 2.0

--- bot/bot_telegram.py
+++ bot/bot_telegram.py
@@ -8,9 +8,25 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 import os
 
-b1 = InlineKeyboardButton('Режим Работы', callback_data='b1')
-kb1 = InlineKeyboardMarkup().add(b1)
 
+b1 = InlineKeyboardButton('Режим Работы', callback_data='b1')
+b2 = InlineKeyboardButton('Записаться', callback_data='b2')
+kb1 = InlineKeyboardMarkup().add(b1).add(b2)
+
+bk1 = InlineKeyboardButton('Первый корт', callback_data='bk1')
+bk2 = InlineKeyboardButton('Второй корт', callback_data='bk2')
+bk3 = InlineKeyboardButton('Третий корт', callback_data='bk3')
+kb2 = InlineKeyboardMarkup().add(bk1).add(bk2).add(bk3)
+
+bt1 = InlineKeyboardButton('Изяслав Ростиславович', callback_data='bt1')
+bt2 = InlineKeyboardButton('Ибанат Магомедович', callback_data='bt2')
+bt3 = InlineKeyboardButton('Елена Чмых', callback_data='bt3')
+bt4 = InlineKeyboardButton('Отказаться от услуг тренера', callback_data='bt4')
+kb3 = InlineKeyboardMarkup().add(bt1).add(bt2).add(bt3).add(bt4)
+
+bi1 = InlineKeyboardButton('Да', callback_data='bi1')
+bi2 = InlineKeyboardButton('Нет', callback_data='bi2')
+kb4 = InlineKeyboardMarkup().add(bi1).add(bi2)
 
 class info(StatesGroup):
     name = State()
@@ -22,9 +38,8 @@ class info(StatesGroup):
     chek = State()
     oplata = State()
 
-
-bot = Bot(token='5034918189:AAF6W5Brq9UOZggdCyQl5r7VV0FT5jx5N9g')
-# bot=Bot(token=os.getenv('TOKEN'))
+bot=Bot(token='5034918189:AAF6W5Brq9UOZggdCyQl5r7VV0FT5jx5N9g')
+#bot=Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
@@ -32,29 +47,26 @@ async def on_startup(_):
     print('Бот в сети')
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start','help'])
 async def commands_start(message: types.Message):
     try:
         await bot.send_message(message.from_user.id, 'Здравствуй, давай запишемся в сквош-клуб', reply_markup=kb1)
+        await message.delete()
     except:
-        await message.reply('Общение с ботом через ЛС, напишите ему: \nhttps://t.me/СпортзальчикBot', reply_markup=kb1)
-
-
-# @dp.message_handler(commands=['Режим_работы'])
-# async def sportzal_open_command(message: types.Message):
-#    await bot.send_message(message.from_user.id, 'Пн-Пт: 7:00-22:00, Сб,Вс: 8:00-21:00')
-
-
-@dp.message_handler(commands=['Записаться'], state=None)
-async def reservation(message: types.Message):
-    await info.name.set()
-    await message.answer('Начнем! \nВведите полное имя')
+        await message.reply('Общение с ботом через ЛС, напишите ему: \nhttps://t.me/СпортзальчикBot')
 
 
 @dp.callback_query_handler(lambda c: c.data == 'b1')
-async def process_callback_button1(callback_query: types.CallbackQuery):
+async def timetable(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Пн-Пт: 7:00-22:00, Сб,Вс: 8:00-21:00')
+
+
+@dp.callback_query_handler(lambda c: c.data == 'b2', state=None)
+async def booking(callback_query: types.CallbackQuery):
+    await info.name.set()
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Начнем! \nВведите ФИО')
 
 
 @dp.message_handler(state=info.name)
@@ -65,50 +77,94 @@ async def get_name(message: types.Message, state: FSMContext):
     await message.reply('Выберите дату бронирования')
 
 
-@dp.message_handler(state=info.date)
+@dp.message_handler(state = info.date)
 async def get_name(message: types.Message, state: FSMContext):
     await info.next()
     await message.answer('Выберите время бронирования')
 
 
-@dp.message_handler(state=info.time)
+@dp.message_handler(state = info.time)
 async def get_name(message: types.Message, state: FSMContext):
     await info.next()
-    await message.answer('Выберите корт')
+    await message.answer('Выберите корт', reply_markup=kb2)
 
 
-@dp.message_handler(state=info.cort)
+@dp.callback_query_handler(lambda c: c.data, state=info.cort)
+async def cort(callback_query: types.CallbackQuery, state: FSMContext):
+    code = callback_query.data[-1]
+    if code == '1':
+        async with state.proxy() as data:
+            data['cort'] = 1
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали 1 корт \nВыберите тренера', reply_markup=kb3)
+    elif code == '2':
+        async with state.proxy() as data:
+            data['cort'] = 2
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали 2 корт \nВыберите тренера', reply_markup=kb3)
+    elif code == '3':
+        async with state.proxy() as data:
+            data['cort'] = 3
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали 3 корт \nВыберите тренера', reply_markup=kb3)
+
+
+@dp.callback_query_handler(lambda c: c.data, state=info.trainer)
+async def cort(callback_query: types.CallbackQuery, state: FSMContext):
+    code = callback_query.data[-1]
+    if code == '1':
+        async with state.proxy() as data:
+            data['trainer'] = 'Изяслав Ростиславович'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали Изяслава Ростиславовича \nНужен ли вам инвентарь?', reply_markup=kb4)
+    elif code == '2':
+        async with state.proxy() as data:
+            data['trainer'] = 'Ибанат Магомедович'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали Ибаната Магомедовича \nНужен ли вам инвентарь?',reply_markup=kb4)
+    elif code == '3':
+        async with state.proxy() as data:
+            data['trainer'] = 'Елена Чмых'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали Елену Чмых \nНужен ли вам инвентарь?',reply_markup=kb4)
+    elif code == '4':
+        async with state.proxy() as data:
+            data['trainer'] = '-'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали отказаться от услуг тренера \nНужен ли вам инвентарь?',reply_markup=kb4)
+
+
+@dp.callback_query_handler(lambda c: c.data, state=info.inventory)
+async def cort(callback_query: types.CallbackQuery, state: FSMContext):
+    code = callback_query.data[-1]
+    if code == '1':
+        async with state.proxy() as data:
+            data['inventory'] = 'Да'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали приобрести инвентарь\n Предоставляю чек')
+    if code == '2':
+        async with state.proxy() as data:
+            data['inventory'] = 'Нет'
+        await info.next()
+        await bot.send_message(callback_query.from_user.id, 'Вы выбрали  не приобретать инвентарь\n Предоставляю чек')
+    await bot.send_message(callback_query.from_user.id, 'ФИО:\nДата и время: \nКорт:\nТренер:\nИнвентарь:')
+    await bot.send_message(callback_query.from_user.id, 'К оплате 100 грн')
+
+
+@dp.message_handler(state = info.oplata)
 async def get_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['cort'] = message.text
-    await info.next()
-    await message.answer('Выберите тренера')
-
-
-@dp.message_handler(state=info.trainer)
-async def get_name(message: types.Message, state: FSMContext):
-    await info.next()
-    await message.answer('Нужен ли Вам инвентарь?')
-
-
-@dp.message_handler(state=info.inventory)
-async def get_name(message: types.Message, state: FSMContext):
-    await info.next()
-    await message.answer('Вот ваш чек')
-
-
-@dp.message_handler(state=info.chek)
-async def get_name(message: types.Message, state: FSMContext):
-    await info.next()
-    await message.answer('К оплате 100 грн')
-
-
-@dp.message_handler(state=info.oplata)
-async def get_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        await message.answer(str(data))
-    await message.answer('Поздравляю')
+    await message.answer('Плати налог')
     await state.finish()
+
+
+
+
+
+
+
+
+
+
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
